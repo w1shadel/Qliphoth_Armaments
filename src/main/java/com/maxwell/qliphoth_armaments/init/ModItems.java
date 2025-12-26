bc8b58ce81c3ba37d7d00a3464fac1d8b1af22db
w1shadel <kik.souta@gmail.com>
2025-12-27 06:26:05 +0900

    a

--- src/main/java/com/maxwell/qliphoth_armaments/init/ModItems.java
+++ src/main/java/com/maxwell/qliphoth_armaments/init/ModItems.java
@@ -23,16 +23,5 @@ public class ModItems {
     public static final Supplier<Item> KNIGHT_SCRAP = ITEMS.register("knight_scrap", () -> new KnightScrapItem(new Item.Properties()));
     public static final Supplier<Item> FIRE_M_BATTLEAXE = ITEMS.register("fire_malkuth_warriors_battle_axe", () -> new SeismicImpactAxeItem(Tiers.DIAMOND, 2, -2.1f, new Item.Properties(), QAElements.FIRE));
     public static final Supplier<Item> ICE_M_BATTLEAXE = ITEMS.register("ice_malkuth_warriors_battle_axe", () -> new SeismicImpactAxeItem(Tiers.DIAMOND, 2, -2.2f, new Item.Properties(), QAElements.ICE));
-    public static final RegistryObject<Item> THE_FRAGMENTOR = ITEMS.register("the_fragmentor",
-            () -> new BaseBedrockItem(
-                    new Item.Properties(),
-                    ModModels.FRAGMENTOR,
-                    new ResourceLocation(QA.MOD_ID, "textures/item/the_fragmentor.png"),
-                    new ItemAnimationConfig(
-                            ModAnims.TEST, 
-                            null,          
-                            null           
-                    )
-            ));
 
 }
